3538d03e28d11f18f1028b3480c26d9528422072
Juliette <juliette.bergoend@laposte.net>
2020-12-04 16:03:44 +0100

    more episodes

--- plumcot_prodigy/recipes.py
+++ plumcot_prodigy/recipes.py
@@ -50,7 +50,7 @@ def stream():
     
     # final list of all episodes
     #episodes_list = [episode.split(',')[0] for episode in all_episodes_series.split('\n')]
-    episodes_list = ["TheBigBangTheory.Season01.Episode09", "TheBigBangTheory.Season01.Episode06", "TheBigBangTheory.Season01.Episode09"]
+    episodes_list = ["BuffyTheVampireSlayer.Season01.Episode01", "BuffyTheVampireSlayer.Season01.Episode02"]
     counter = 0
     
     while counter < len(episodes_list):
