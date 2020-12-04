be12b2eba6996e3096fdf4b0a00979e2b919b91a
Juliette <juliette.bergoend@laposte.net>
2020-12-04 15:54:11 +0100

    Test on multiple episodes V1.5 - more episodes

--- plumcot_prodigy/recipes.py
+++ plumcot_prodigy/recipes.py
@@ -50,7 +50,7 @@ def stream():
     
     # final list of all episodes
     #episodes_list = [episode.split(',')[0] for episode in all_episodes_series.split('\n')]
-    episodes_list = ["TheBigBangTheory.Season01.Episode01", "TheBigBangTheory.Season01.Episode03"]
+    episodes_list = ["TheBigBangTheory.Season01.Episode09", "TheBigBangTheory.Season01.Episode06", "TheBigBangTheory.Season01.Episode09"]
     counter = 0
     
     while counter < len(episodes_list):
