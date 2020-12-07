0821f5cc997166f0b603aafa6dc43741b3a25a09
Hervé BREDIN <hbredin@users.noreply.github.com>
2020-12-07 09:28:46 +0100

    fix: remove nested loop

--- plumcot_prodigy/recipes.py
+++ plumcot_prodigy/recipes.py
@@ -51,62 +51,57 @@ def stream():
     #episodes_list = [episode.split(',')[0] for episode in all_episodes_series.split('\n')]
     episodes_list = ["TheBigBangTheory.Season01.Episode04", "TheBigBangTheory.Season01.Episode05"]
     
-    counter = 0
-    
-    while counter < len(episodes_list):
-    
-        for episode in episodes_list:    
-            series, _, _ = episode.split('.')
-            
-            # path to mkv -- hardcoded for now
-            mkv = f"/vol/work3/lefevre/dvd_extracted/{series}/{episode}.mkv"
-    
-            # path to forced alignment -- hardcoded for now
-            aligned = f"/vol/work/lerner/pyannote-db-plumcot/Plumcot/data/{series}/forced-alignment/{episode}.aligned"
+    for episode in episodes_list:    
+        series, _, _ = episode.split('.')
 
-            # load forced alignment        
-            transcript = forced_alignment(aligned)      
-            sentences = list(transcript.sents)
+        # path to mkv -- hardcoded for now
+        mkv = f"/vol/work3/lefevre/dvd_extracted/{series}/{episode}.mkv"
 
-            # select the first and the last sentences of the episode
-            sentence_begining = sentences[0]
-            sentence_end = sentences[-1]
+        # path to forced alignment -- hardcoded for now
+        aligned = f"/vol/work/lerner/pyannote-db-plumcot/Plumcot/data/{series}/forced-alignment/{episode}.aligned"
 
-            # load its attributes from forced alignment
-            speaker = sentence_begining._.speaker
-            print(speaker)
-            start_time_b = sentence_begining._.start_time
-            print(start_time_b)
-            end_time_b = sentence_begining._.end_time
-            print(end_time_b)
-            
-            # extract corresponding video excerpt
-            video_excerpt_b = mkv_to_base64(mkv, start_time_b, end_time_b)
-            
-            yield {
-                "video": video_excerpt_b,
-                "text": f"{speaker}: {sentence_begining}",
-                "meta": {"start": start_time_b, "end": end_time_b, "episode": episode},
-            }
-            
-            # load its attributes from forced alignment
-            speaker = sentence_end._.speaker
-            print(speaker)
-            start_time = sentence_end._.start_time
-            print(start_time)
-            end_time = sentence_end._.end_time
-            print(end_time)
-            
-            # extract corresponding video excerpt
-            video_excerpt_e = mkv_to_base64(mkv, start_time, end_time)
-            #print("Extrait video", type(video_excerpt))
-            
-            yield {
-                "video": video_excerpt_e,
-                "text": f"{speaker}: {sentence_end}",
-                "meta": {"start": start_time, "end": end_time, "episode": episode},
-            }
-            counter +=1
+        # load forced alignment        
+        transcript = forced_alignment(aligned)      
+        sentences = list(transcript.sents)
+
+        # select the first and the last sentences of the episode
+        sentence_begining = sentences[0]
+        sentence_end = sentences[-1]
+
+        # load its attributes from forced alignment
+        speaker = sentence_begining._.speaker
+        print(speaker)
+        start_time_b = sentence_begining._.start_time
+        print(start_time_b)
+        end_time_b = sentence_begining._.end_time
+        print(end_time_b)
+
+        # extract corresponding video excerpt
+        video_excerpt_b = mkv_to_base64(mkv, start_time_b, end_time_b)
+
+        yield {
+            "video": video_excerpt_b,
+            "text": f"{speaker}: {sentence_begining}",
+            "meta": {"start": start_time_b, "end": end_time_b, "episode": episode},
+        }
+
+        # load its attributes from forced alignment
+        speaker = sentence_end._.speaker
+        print(speaker)
+        start_time = sentence_end._.start_time
+        print(start_time)
+        end_time = sentence_end._.end_time
+        print(end_time)
+
+        # extract corresponding video excerpt
+        video_excerpt_e = mkv_to_base64(mkv, start_time, end_time)
+        #print("Extrait video", type(video_excerpt))
+
+        yield {
+            "video": video_excerpt_e,
+            "text": f"{speaker}: {sentence_end}",
+            "meta": {"start": start_time, "end": end_time, "episode": episode},
+        }
 
 
 @prodigy.recipe(
